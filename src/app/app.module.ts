774dfed6658b16ff339ce60ea32f7b373127a8dc
gnganpath <gnganapathi@gmail.com>
2018-04-08 22:29:01 +0530

    angular 2 material imple

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './app.routes';
+
 
 import { AppComponent } from './app.component';
 import { CircularComponent } from './circular/circular.component';
 import { SearchComponent } from './search/search.component';
 import {GithubService} from './github/github.service';
+import {MatCardModule} from '@angular/material/card';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CircularComponent,
-    SearchComponent
+    SearchComponent,
+    UserComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
+ 
   ],
   providers: [GithubService],
   bootstrap: [AppComponent]
